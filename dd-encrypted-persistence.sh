f6862e6ee0e74b1d22fbcd7a5fe1657066c8821a
s3Gf4ulT <huffmanj573@gmail.com>
2023-05-17 12:47:40 -0400

    Create dd-encrypted-persistence.sh

---
+++ dd-encrypted-persistence.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script automates the process of copying the ctf-live iso to /dev/sdb with encrypted persistence
+# Not responsible if you wipe the wrong device
+
+echo "Enter name of device to image (default: sdb)"
+read devname
+devname="${devname:=sdb}"
+echo "[+] Copying iso to Disk /dev/$devname"
+dd if=./images/kali-linux-ctf-live-amd64.iso of=/dev/$devname conv=fsync bs=4M status=progress && echo "[+] Sleeping for 15 seconds" && sleep 15 && echo "[+] Creating new partion /dev/$devname" && fdisk /dev/$devname <<< $(printf "n\np\n\n\n\nw")
+echo "[+] Sleeping for 15 seconds"
+sleep 15
+echo "[+] Displaying Blocks"
+lsblk
+echo "[+] Setting up crypt"
+cryptsetup --batch-mode --verbose --verify-passphrase luksFormat /dev/$devname3
+cryptsetup luksOpen /dev/$devname3 my_usb
+echo "[+] Creating ext4 File System"
+mkfs.ext4 -L persistence /dev/mapper/my_usb
+e2label /dev/mapper/my_usb persistence
+echo "[+] Creating Persistence conf file"
+mkdir -p /mnt/my_usb
+mount /dev/mapper/my_usb /mnt/my_usb
+echo "/ union" | sudo tee /mnt/my_usb/persistence.conf
+echo "[+] Cleaning up"
+umount /dev/mapper/my_usb
+cryptsetup luksClose /dev/mapper/my_usb
+echo "[+] Done"
